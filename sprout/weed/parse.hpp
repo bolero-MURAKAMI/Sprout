e3511d71b091f93d692e7b8bfa3948fc94b4bf3e
bolero-MURAKAMI <bolero.murakami@gmail.com>
2012-07-23 20:39:03 +0900

    add weed::parse_range

--- sprout/weed/parse.hpp
+++ sprout/weed/parse.hpp
@@ -2,6 +2,8 @@
 #define SPROUT_WEED_PARSE_HPP
 
 #include <sprout/config.hpp>
+#include <sprout/container/functions.hpp>
+#include <sprout/container/traits.hpp>
 #include <sprout/weed/expr/make_terminal_or_expr.hpp>
 #include <sprout/weed/context/parse_context.hpp>
 #include <sprout/weed/traits/expr/terminal_or_expr_of.hpp>
@@ -12,19 +14,30 @@ namespace sprout {
 		// parse
 		//
 		template<typename Iterator, typename Parser>
-		inline SPROUT_CONSTEXPR typename sprout::weed::parse_context<Iterator>::template eval<
+		inline SPROUT_CONSTEXPR typename sprout::weed::parse_context<
+			Iterator
+		>::template eval<
 			typename sprout::weed::traits::terminal_or_expr_of<Parser>::type
-		>::result_type::presult_type parse(
-			Iterator first,
-			Iterator last,
-			Parser const& parser
-			)
-		{
+		>::result_type::presult_type
+		parse(Iterator first, Iterator last, Parser const& parser) {
 			return sprout::weed::eval(
 				sprout::weed::make_terminal_or_expr(parser),
 				sprout::weed::parse_context<Iterator>(first, last)
 				).presult()
 				;
+		}
+
+		//
+		// parse_range
+		//
+		template<typename Range, typename Parser>
+		inline SPROUT_CONSTEXPR typename sprout::weed::parse_context<
+			typename sprout::container_traits<Range>::const_iterator
+		>::template eval<
+			typename sprout::weed::traits::terminal_or_expr_of<Parser>::type
+		>::result_type::presult_type
+		parse_range(Range const& range, Parser const& parser) {
+			return sprout::weed::parse(sprout::begin(range), sprout::end(range), parser);
 		}
 	}	// namespace weed
 }	// namespace sprout
