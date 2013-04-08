ef5aa728b09a161a19d3ad92c22dbb3573cca8d3
bolero-MURAKAMI <bolero.murakami@gmail.com>
2013-04-08 17:45:03 +0900

    fix string_ref io

--- sprout/utility/string_ref/io.hpp
+++ sprout/utility/string_ref/io.hpp
@@ -1,6 +1,8 @@
 #ifndef SPROUT_UTILITY_STRING_IO_HPP
 #define SPROUT_UTILITY_STRING_IO_HPP
 
+#include <iterator>
+#include <algorithm>
 #include <ios>
 #include <sprout/config.hpp>
 #include <sprout/utility/string_ref/string_ref.hpp>
@@ -12,7 +14,8 @@ namespace sprout {
 	template<typename T, typename Traits, typename StreamTraits>
 	inline std::basic_ostream<T, StreamTraits>&
 	operator<<(std::basic_ostream<T, StreamTraits>& lhs, sprout::basic_string_ref<T, Traits> const& rhs) {
-		return lhs << rhs.c_str();
+		std::copy(rhs.begin(), rhs.end(), std::ostreambuf_iterator<T, StreamTraits>(lhs));
+		return lhs;
 	}
 }	// namespace sprout
 
