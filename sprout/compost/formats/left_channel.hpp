14f8d9d0424091b6d7cf2f8bd8d80f76fa521aaa
bolero-MURAKAMI <bolero.murakami@gmail.com>
2014-01-08 16:48:12 +0900

    fix copyright year

--- sprout/compost/formats/left_channel.hpp
+++ sprout/compost/formats/left_channel.hpp
@@ -1,5 +1,5 @@
 /*=============================================================================
-  Copyright (c) 2011-2013 Bolero MURAKAMI
+  Copyright (c) 2011-2014 Bolero MURAKAMI
   https://github.com/bolero-MURAKAMI/Sprout
 
   Distributed under the Boost Software License, Version 1.0. (See accompanying
